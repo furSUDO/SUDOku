28c088b40dce0617fbe3f994484a8e3715e7284f
furSUDO <tommysmurray@gmail.com>
2020-09-15 13:54:28 +0200

    hehe

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 
-const client = new Discord.Client();
+const client = new Discord.Client({partials: ['MESSAGE','CHANNEL','REACTION']});
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
