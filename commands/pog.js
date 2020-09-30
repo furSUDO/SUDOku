47607f09aae6660d1af081d632d2377e379ab327
furSUDO <tommysmurray@gmail.com>
2020-09-30 12:37:20 +0200

    got canvas working

--- commands/pog.js
+++ commands/pog.js
@@ -1,12 +1,13 @@
 const jsonfile = require ('jsonfile');
 const fs = require('fs');
 const Canvas = require('canvas');
+const Discord = require('discord.js');
 
 module.exports = {
 	name: 'pog',
 	description: 'Shows user level',
 	cooldown: 5,
-	execute(message) {
+	async execute(message) {
 		var stats ={};
 		if (fs.existsSync('levels.json')) {
 			stats = jsonfile.readFileSync('levels.json')
@@ -17,13 +18,11 @@ module.exports = {
 		
 		const canvas = Canvas.createCanvas(700, 250);
 		const ctx = canvas.getContext('2d');
-		
-		//This is the line that needs to be asynchronous 
-		const background = Canvas.loadImage('./background1.png');	
+		// we need to await the Promise gets resolved since loading of Image is async
+		const background = await Canvas.loadImage('./assets/background1.png');
 
-		ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-		const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'level.png');
-		
-		channel.send(`Welcome to the server, ${member}!`, attachment);
+		ctx.drawImage(background, 0, 0, canvas.width, canvas.height);  
+		const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'test.png');
+		message.channel.send(`Testing...`, attachment);
 	}
 };
