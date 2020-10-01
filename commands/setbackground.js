91cff52ab7c5f5c4c204b06fa4f0a4c39a7f1ec8
furSUDO <tommysmurray@gmail.com>
2020-10-01 14:00:39 +0200

    init

---
+++ commands/setbackground.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const jsonfile = require ('jsonfile');
+
+module.exports = {
+	name: 'setbackground',
+	description: 'Ping!',
+	cooldown: 5,
+	execute(message,args) {
+		args = args.join();
+		console.log(args);
+		var stats ={};
+		if (fs.existsSync('levels.json')) {
+			stats = jsonfile.readFileSync('levels.json')
+		}
+		const guildStats = stats[message.guild.id];
+		const userStats = guildStats[message.author.id];
+		if (args=='') {
+			message.reply("You haven't selected a background.\nPlease choose a number between ``0 and 12``")
+		} else {
+			userStats.background = args
+		}
+		jsonfile.writeFileSync('levels.json',stats)
+	},
+};
