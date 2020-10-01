cbc9953a356f871cb3176d121b5090aed4501296
furSUDO <tommysmurray@gmail.com>
2020-10-01 11:48:13 +0200

    made the test version easy to lvl up

--- leveling.js
+++ leveling.js
@@ -63,47 +63,47 @@ client.on('message', async message => {
     }
     
     const userStats = guildStats[message.author.id]
-    //if (Date.now()-userStats.last_message > 60000) {
-    //    
-    //        userStats.xp += Math.floor(Math.random()*7)+8;
-    //        userStats.last_message = Date.now();
-    //    
-    //        const xpToNextLevel = 5 * Math.pow(userStats.level,2)+50*userStats.level+100;
-    //        if (userStats.xp >= xpToNextLevel) {
-    //            userStats.level++;
-    //            userStats.xp = userStats.xp - xpToNextLevel;
-    //            message.channel.send(`${displayName} has reached level ${userStats.level}`)
-    //        }
-    //    
-    //        jsonfile.writeFileSync('levels.json',stats)
-    //        
-    //        console.log(`${displayName} now has ${userStats.xp}`);
-    //        console.log(`${xpToNextLevel} needed to lvl up`);
-    //    
-    //}
+    if (Date.now()-userStats.last_message > 100) {
+        
+            userStats.xp += Math.floor(Math.random()*7)+500;
+            userStats.last_message = Date.now();
+        
+            const xpToNextLevel = 5 * Math.pow(userStats.level,2)+50*userStats.level;
+            if (userStats.xp >= xpToNextLevel) {
+                userStats.level++;
+                userStats.xp = userStats.xp - xpToNextLevel;
+                console.log(`${displayName(message)} has reached level ${userStats.level}`)
+            }
+        
+            jsonfile.writeFileSync('levels.json',stats)
+            
+            console.log(`${displayName(message)} now has ${userStats.xp}`);
+            console.log(`${xpToNextLevel} needed to lvl up`);
+        
+    }
 	console.log(message.content);
-	const regex2 = /(?=.*<@!480495309491798037>)/im;
-	const regex = /(?=.*SUDO)(?=.*is)(?=.*cute)/im;
-	let m;
-	if (!message.guild) return;
-	if ((m = regex.exec(message.content)) !== null) {
-		message.delete();
-		let channel = message.channel;
-		channel.createInvite({unique: true}).then(invite => {
-			message.author.send(`You have been kicked from the server for calling SUDO cute.\nHere is a link to join back ${invite}`);
-		}).catch((err) => {
-			
-		});
-		message.reply
-		message.member.kick().then((member) => {
-			// Successmessage
-			message.channel.send(`Nuuuuu! >:C\n${member} was kicked for calling SUDO cute`);
-			message.channel.send(`<:SUDO_L_Knife_1:756994302621515786><:SUDO_L_Knife_2:756994315515068486>\n<:SUDO_L_Knife_3:756994334108155995><:SUDO_L_Knife_4:756994347550900275>`);
-        }).catch(() => {
-             // Failmessage
-            console.log("oh shit oh fuck");
-        });
-	}
+	//const regex2 = /(?=.*<@!480495309491798037>)/im;
+	//const regex = /(?=.*SUDO)(?=.*is)(?=.*cute)/im;
+	//let m;
+	//if (!message.guild) return;
+	//if ((m = regex.exec(message.content)) !== null) {
+	//	message.delete();
+	//	let channel = message.channel;
+	//	channel.createInvite({unique: true}).then(invite => {
+	//		message.author.send(`You have been kicked from the server for calling SUDO cute.\nHere is a link to join back ${invite}`);
+	//	}).catch((err) => {
+	//		
+	//	});
+	//	message.reply
+	//	message.member.kick().then((member) => {
+	//		// Successmessage
+	//		message.channel.send(`Nuuuuu! >:C\n${member} was kicked for calling SUDO cute`);
+	//		message.channel.send(`<:SUDO_L_Knife_1:756994302621515786><:SUDO_L_Knife_2:756994315515068486>\n<:SUDO_L_Knife_3:756994334108155995><:SUDO_L_Knife_4:756994347550900275>`);
+    //    }).catch(() => {
+    //         // Failmessage
+    //        console.log("oh shit oh fuck");
+    //    });
+	//}
 
 	//if ((m = regex.exec(message.content)) !== null) {
 	//	let channel = message.channel;
