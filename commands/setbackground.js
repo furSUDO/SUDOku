bfd9934f9ba604cc32d2f042d481f16d8b3ed3bc
furSUDO <tommysmurray@gmail.com>
2020-10-02 11:14:08 +0200

    flattened code

--- commands/setbackground.js
+++ commands/setbackground.js
@@ -18,8 +18,8 @@ module.exports = {
 		if (args=='') {
 			message.reply("You haven't selected a background.\nPlease choose a number between ``0 and 12``")
 		} else {
-			userStats.background = args
+			if (args >= 0 && <= 15) {userStats.background = args;jsonfile.writeFileSync('levels.json',stats);}
+			else{message.reply(`${args} is not a number between 0 and 15`)};
 		}
-		jsonfile.writeFileSync('levels.json',stats)
 	},
 };
