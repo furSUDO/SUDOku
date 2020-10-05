148e7e15bd3c4a988f70c15309f17e3b2b6e319a
furSUDO <tommysmurray@gmail.com>
2020-10-05 17:28:15 +0200

    removed

--- commands/amazon.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'amazon',
-	description: 'I am trying to get a .bot domain',
-	cooldown: 5,
-	execute(message) {
-		message.channel.send('Response for .bot domain');
-	},
-};
