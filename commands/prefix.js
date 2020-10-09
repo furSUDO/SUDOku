85dbdf4ec9e0b919f244925f37b08a79c350a981
furSUDO <tommysmurray@gmail.com>
2020-10-09 11:15:45 +0200

    prefix command

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,8 +1,22 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const jsonfile = require ('jsonfile');
+
 module.exports = {
 	name: 'prefix',
 	description: 'Change the prefix of SUDOku',
-	cooldown: 5,
-	execute(message) {
-		message.channel.send('Pong.');
+	cooldown: 30,
+	adminOnly: true,
+	args: true,
+	usage: 'newPrefix',
+	execute(message,args) {
+		var settings={};
+			if (fs.existsSync('settings.json')) {
+				settings = jsonfile.readFileSync('settings.json')
+			}
+			const guildSettings = settings[message.guild.id];
+			guildSettings.prefix = args[0]
+			message.reply(`You have changed the prefix for SUDOku to `+"``"+args[0]+"``")
+			jsonfile.writeFileSync('settings.json',settings)
 	},
 };
